e9795d2f6341b6b49b99dab5961ff275990200de
Zahid Toruk <150917933+ZahidBahadirToruk@users.noreply.github.com>
2024-06-25 13:31:50 +0300

    main update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from library import Library
 from book import Book
+from patron import Patron
+
 
 our_library = Library('Wseiz Library', 'Rejtana 17, Warsaw')
 our_book = Book('Robinson Cruzeo', 300, 'adventure', 'J.J Rowling', '3423-kdsjflkdsjfkl')
